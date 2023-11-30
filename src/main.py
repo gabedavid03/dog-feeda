71a99700fa3d0f6e5aea0c798b221f0951705bce
Gabe David <20gad4@queensu.ca>
2023-11-30 00:38:32 -0500

    main.py baby

--- src/main.py
+++ src/main.py
@@ -1,34 +1,15 @@
 import cv2
 import time
-from controller import send_feed, COMPORT
+from controller import send_feed, feed_now, reset_completed, autofeed, COMPORT, validate_feed_time, retrieve_cycles
+import time
 
-def isValid(detection): 
-    detection_start_time = time.time()
-    detection_threshold = 2 #this is seconds you want to wait until dawg detected 
-    detection_valid = False 
-    print(f"MAMA WE MADE IT")
-    while(1):
-        if detection: 
-            time.sleep(3) # buffer to get the detection_start_time to catch up to threshold 
-            stopwatch = time.time() - detection_start_time
-            print(f"{stopwatch}")
-            if  stopwatch >= detection_threshold: 
-                detection_valid = True 
-                print(f"DAWG_STATUS: {detection_valid}")
-                send_feed(COMPORT, )
-                break
-            else: 
-                print(f"DAWG_STATUS: {detection_valid}")
-                detection_start_time = 0
-                detection_valid = False 
-                break     
-   
-#opencv DNN
+
+#opencv DNN variables
 net = cv2.dnn.readNet("dnn_model/yolov4-tiny.weights", "dnn_model/yolov4-tiny.cfg")
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(320,320), scale=1/255)
 
-#load class list
+#load class list for object detection
 classes = []
 with open("dnn_model/classes.txt", "r") as file_object:
     for class_name in file_object.readlines():
@@ -40,10 +21,8 @@ print(classes)
 #initialize camera
 cap = cv2.VideoCapture(0)
 
-#create a window
-
-while True:
-    #get frames
+def object_detection() -> None: 
+        #get frames
     ret, frame = cap.read()
 
     #object detection
@@ -63,9 +42,43 @@ while True:
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     print(key)
-    if key == 113:
-        break
+    return
 
-# cap.release()
-# cv2.destroyAllWindows()
+def isValid(detection: bool) -> None: 
+    detection_start_time = time.time()
+    detection_threshold = 2 #this is seconds you want to wait until dawg detected 
+    detection_valid = False 
+    print(f"MAMA WE MADE IT")
+    while True:
+        if detection: 
+            time.sleep(3) # buffer to get the detection_start_time to catch up to threshold 
+            stopwatch = time.time() - detection_start_time
+            print(f"{stopwatch}")
+            if  stopwatch >= detection_threshold: 
+                detection_valid = True 
+                print(f"DAWG_STATUS: {detection_valid}")
+                valid = validate_feed_time()
+                if (valid != 0): 
+                    cycles = retrieve_cycles(valid) 
+                    send_feed(COMPORT, cycles)
+                break
+            else: 
+                print(f"DAWG_STATUS: {detection_valid}")
+                detection_start_time = 0
+                detection_valid = False 
+                break     
+   
+# while loop where all of the code run
 
+def main(args=None) -> None:
+    while True: 
+        feed_now()
+        reset_completed()
+        autofeed() 
+        object_detection()
+
+if __name__ == '__main__':
+        try:
+            main()
+        except Exception as e:
+            print(e)
