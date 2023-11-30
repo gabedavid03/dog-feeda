71869241b89d2bfa369d82f1d0348307509a5f28
ryanberry13 <ryanberry1303@gmail.com>
2023-11-29 23:04:00 -0500

    first commit for adding time validation

--- web_server/app.py
+++ web_server/app.py
@@ -3,7 +3,6 @@ import json
 
 app = Flask(__name__)
 
-# Path to the JSON file where feed details are stored
 JSON_FILE_PATH = 'feeds.json'
 
 def read_feeds():
@@ -19,29 +18,28 @@ def write_feeds(data):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    feeds_data = read_feeds()
+
     if request.method == 'POST':
-        feeds_data = read_feeds()
-        
-        if 'delete' in request.form:
-            feed_number_to_delete = int(request.form.get('delete'))
-            feeds_data['feeds'] = [feed for feed in feeds_data['feeds'] if feed['feed_number'] != feed_number_to_delete]
-        
-        elif 'add_feed' in request.form:
+        if 'add_feed' in request.form:
             new_feed = {
                 "feed_number": len(feeds_data['feeds']) + 1,
                 "start_time": request.form.get('start_time'),
                 "end_time": request.form.get('end_time'),
-                "amount": float(request.form.get('amount'))
+                "amount": float(request.form.get('amount')),
+                "completed": 0
             }
             feeds_data['feeds'].append(new_feed)
-        
+
+        elif 'delete' in request.form:
+            feed_number_to_delete = int(request.form.get('delete'))
+            feeds_data['feeds'] = [feed for feed in feeds_data['feeds'] if feed['feed_number'] != feed_number_to_delete]
+
         feeds_data['autodispense'] = 1 if 'autodispense' in request.form else 0
-        
+
         write_feeds(feeds_data)
-        
         return redirect(url_for('index'))
 
-    feeds_data = read_feeds()
     return render_template('index.html', feeds=feeds_data['feeds'], autodispense=feeds_data['autodispense'])
 
 if __name__ == '__main__':
